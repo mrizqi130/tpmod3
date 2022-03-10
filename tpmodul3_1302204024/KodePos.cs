8266de363ce2d1b915439027c0710bf6b09c5ff9
RizqiAnshari <muhammadrizqia@student.telkomuniversity.ac.id>
2022-03-11 04:45:57 +0800

    menambahkan implementasi dengan table-driven

---
+++ tpmodul3_1302204024/KodePos.cs
@@ -0,0 +1,143 @@
+﻿using System;
+
+namespace tpmodul3_1302204024
+{
+	//belajar passbyvalue passbyref
+	public class KodePos
+	{
+		//string b = "rara"; //jika tidak menggunakan public sbg modifier maka b mjd var global pada class ini saja
+		public string b = "ini class KodePos";
+        //not all code paths return value -> ketika return value tidak diset pada func
+        enum State
+        {
+            START, GAME, PAUSE, EXIT,
+            Batununggal, Kujangsari, Mengger, Wates,
+            Cijaura, Jatisari, Margasari, Sekejati, Kebonwaru,
+            Maleer, Samoja
+        };
+        //Batununggal
+        //Kujangsari
+        //Mengger
+        //Wates
+        //Cijaura
+        //Jatisari
+        //Margasari
+        //Sekejati
+        //Kebonwaru
+        //Maleer
+        //Samoja
+        public void getKodePos()
+        {
+
+            State state = State.START;
+            State kel = State.START;
+            string[] screenName = { "START", "GAME", "PAUSE", "EXIT", "40266",
+                "40287", "40267", "40256", "40287", "40286", "40286", "40286",
+                "40272", "40274", "40273"
+
+            };
+
+            while (state != State.EXIT)
+            {
+                //Console.WriteLine(screenName[(int)state] + " SCREEN");
+                Console.WriteLine("Ketikkan kelurahan untuk menampilkan kode pos");
+                Console.Write("Kelurahan : ");
+                //KONSTRUKSI PERANGKAT LUNAK 17
+
+                //string command = Console.ReadLine();
+                string command = Console.ReadLine();
+                switch (state)
+                {
+                    case State.START:
+                        if (command == "ENTER")
+                        {
+                            state = State.GAME;
+                        }
+                        else if (command == "QUIT")
+                        {
+                            state = State.EXIT;
+                        }
+                        else if (command == "Batununggal")
+                        {
+                            //Console.Write("INI BATUNUNGGAL");
+                            kel = State.Batununggal;
+                        }
+                        else if (command == "Kujangsari")
+                        {
+                            kel = State.Kujangsari;
+                        }
+                        else if (command == "Mengger")
+                        {
+                            kel = State.Mengger;
+                        }
+                        else if (command == "Wates")
+                        {
+                            kel = State.Wates;
+                        }
+                        else if (command == "Cijaura")
+                        {
+                            kel = State.Cijaura;
+                        }
+                        else if (command == "Jatisari")
+                        {
+                            kel = State.Jatisari;
+                        }
+                        else if (command == "Margasari")
+                        {
+                            kel = State.Margasari;
+                        }
+                        else if (command == "Sekejati")
+                        {
+                            kel = State.Sekejati;
+                        }
+                        else if (command == "Kebonwaru")
+                        {
+                            kel = State.Kebonwaru;
+                        }
+                        else if (command == "Maleer")
+                        {
+                            kel = State.Maleer;
+                        }
+                        else if (command == "Samoja")
+                        {
+                            kel = State.Samoja;
+                        }
+                        else
+                        {
+                            state = State.START;
+                            command = "QUIT";
+                            
+                        }
+                        break;
+                        //case State.Batununggal:
+                        //    if (command == "ESC")
+                        //        state = State.PAUSE;
+                        //    else
+                        //        state = State.GAME;
+                        //    break;
+                        //case State.GAME:
+                        //    if (command == "ESC")
+                        //        state = State.PAUSE;
+                        //    else
+                        //        state = State.GAME;
+                        //    break;
+                        //case State.PAUSE:
+                        //    if (command == "BACK")
+                        //        state = State.GAME;
+                        //    else if (command == "HOME")
+                        //        state = State.START;
+                        //    else if (command == "QUIT")
+                        //        state = State.EXIT;
+                        //    else
+                        //        state = State.PAUSE;
+                        //    break;
+                }
+                if (command != "QUIT")
+                    //a = screenName[(int)kel];
+                    Console.WriteLine("Kode Pos " + command + " : " + screenName[(int)kel]);
+                //Console.WriteLine("yes1" + command);
+            }
+            Console.WriteLine("EXIT SCREEN");
+        }
+    }
+}
--- tpmodul3_1302204024/Program.cs
+++ tpmodul3_1302204024/Program.cs
@@ -1,15 +1,47 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace tpmodul3_1302204024
 {
-    internal class Program
+    //intern
+    //al class Program
+    public class Program
     {
-        static void Main(string[] args)
+        //main harus pakai static
+        public static void Main(string[] args)
         {
+            KodePos kode = new KodePos(); //constructor yg mengembalikan keseluruhan class include attrib n method
+
+            Console.WriteLine();
+            kode.getKodePos();
+
+            //string b = "Samaja";
+            //Console.WriteLine("");
+            //Console.WriteLine(kode.getKodePos(b));
+
+            //Console.WriteLine("");
+            //if (b == "1")
+            //    Console.WriteLine("b");
+            //else if (b == "2")
+            //    Console.WriteLine(b);
+            //Console.WriteLine(b);
+            //else if (b == "3")
+            //    Console.WriteLine(b);
+
+            //String[] a = { "1", "2", "3" };
+        }
+
+    }
+    public class tess
+    {
+        // harus memiliki modifier public agar dapat diakses leh class program
+        public string aa = "ini atribute kelas tess pada file yang sama";
+        //access modifier + permitted modifier
+        //static tidak bisa diakses oleh class lain pada file yg sama
+        public void tes()
+        {
+            Console.WriteLine("yey ini void tes()");
         }
     }
 }
+//comment ctrl+k + ctrl+c
+//shift+alt+.
